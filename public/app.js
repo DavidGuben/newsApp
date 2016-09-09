152281bbda9cab8dfc5040def62c17e849a1171f
DavidGuben <davidjg6@gmail.com>
2016-09-09 13:39:08 -0400

    all articles display in thier own box layed out on the screen

--- public/app.js
+++ public/app.js
@@ -4,9 +4,9 @@ $( document ).ready(function() {
     $.getJSON('/all', function(data) {
       for(var i = 0; i < data.length; i++) {
         console.log(data[i]);
-        $('#results').append(
+        $('#results').append('<div class="col-md-4">'+
           '<p data-id='+data[i]._id+'><span class="dataTitle">'+data[i].title+'</span></p>'+
-          '<a target="_blank"href='+data[i].link+'data-id='+data[i]._id+'><span class="dataLink">'+data[i].link+'</span></p>'
+          '<a target="_blank"href='+data[i].link+'data-id='+data[i]._id+'><span class="dataLink">'+data[i].link+'</span></p></div>'
         );
       }
     });
