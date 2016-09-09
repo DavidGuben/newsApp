b978a1420a64da220353decaecda66030d08320d
DavidGuben <davidjg6@gmail.com>
2016-09-09 13:52:14 -0400

    nicely laid out boxes and cleaned up code. Cleaned up article links as well

--- public/app.js
+++ public/app.js
@@ -4,9 +4,11 @@ $( document ).ready(function() {
     $.getJSON('/all', function(data) {
       for(var i = 0; i < data.length; i++) {
         console.log(data[i]);
-        $('#results').append('<div class="col-md-4">'+
-          '<p data-id='+data[i]._id+'><span class="dataTitle">'+data[i].title+'</span></p>'+
-          '<a target="_blank"href='+data[i].link+'data-id='+data[i]._id+'><span class="dataLink">'+data[i].link+'</span></p></div>'
+        $('#results').append(
+          '<div class="col-md-4 boxStyle">'+
+          '<p data-id='+data[i]._id+'><span class="dataTitle">'+data[i].title+
+          '</span></p>'+
+          '<a target="_blank"href='+data[i].link+'data-id='+data[i]._id+'><span class="dataLink">Go to article</span></p></div>'
         );
       }
     });
